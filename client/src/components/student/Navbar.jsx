2cd7554a0f4b06855de3cdaf3f769afb50ce6a84
Sanjaya Prasad <130231733+SanjayaPrasadRajapaksha@users.noreply.github.com>
2025-04-15 22:13:13 +0530

    Add responsive design to navbar

--- client/src/components/student/Navbar.jsx
+++ client/src/components/student/Navbar.jsx
@@ -11,12 +11,21 @@ const Navbar = () => {
     border-gray-500 py-4 ${isCourseListPage ? 'bg-white' : 'bg-cyan-100/70'}`}>
             <img src={assets.logo} alt='Logo' className='w-28 lg:w-32 cursor-pointer' />
             <div className='hidden md:flex items-center gap-5 text-gray-500'>
-                <div>
+                <div className='flex items-center gap-5'>
                     <button>Become Educator</button>
                     <Link to='/my-enrollments'>My Enrollments</Link>
                 </div>
                 <button className='bg-blue-600 text-white px-5 py-2 rounded-full'>Create Acconut</button>
             </div>
+            {/* For mobile screens */}
+            <div className='md:hidden flex items-center gap-2 sm:gap-5 text-gray-500'>
+                <div>
+                    <button>Become Educator</button>
+                    <Link to='/my-enrollments'>My Enrollments</Link>
+                </div>
+                <button><img src={assets.user_icon} alt="" /></button>
+            </div>
+
         </div>
     )
 }
