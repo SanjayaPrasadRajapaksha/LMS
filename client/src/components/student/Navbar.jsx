e092ed9c3cf7b6ff1f320e5fc4ced851ebc6e619
Sanjaya Prasad <130231733+SanjayaPrasadRajapaksha@users.noreply.github.com>
2025-04-15 23:04:29 +0530

    Add user-only educator and enrollment links

--- client/src/components/student/Navbar.jsx
+++ client/src/components/student/Navbar.jsx
@@ -8,18 +8,20 @@ const Navbar = () => {
     const location = useLocation();
     const isCourseListPage = location.pathname.includes('/course-list');
 
-    const {openSignIn} = useClerk();
-    const {user} = useUser();
+    const { openSignIn } = useClerk();
+    const { user } = useUser();
     return (
         <div className={`flex items-center justify-between px-4 sm:px-10 md:px-14 lg:px-36 border-b
     border-gray-500 py-4 ${isCourseListPage ? 'bg-white' : 'bg-cyan-100/70'}`}>
             <img src={assets.logo} alt='Logo' className='w-28 lg:w-32 cursor-pointer' />
             <div className='hidden md:flex items-center gap-5 text-gray-500'>
                 <div className='flex items-center gap-5'>
-                    <button>Become Educator</button>
-                    <Link to='/my-enrollments'>My Enrollments</Link>
+                    {user && <>
+                        <button>Become Educator</button>
+                        <Link to='/my-enrollments'>My Enrollments</Link>
+                    </>}
                 </div>
-                {user ? <UserButton/> : <button onClick={()=> openSignIn()} className='bg-blue-600 text-white px-5 py-2 rounded-full'>Create Acconut</button>}
+                {user ? <UserButton /> : <button onClick={() => openSignIn()} className='bg-blue-600 text-white px-5 py-2 rounded-full'>Create Acconut</button>}
             </div>
             {/* For mobile screens */}
             <div className='md:hidden flex items-center gap-2 sm:gap-5 text-gray-500'>
