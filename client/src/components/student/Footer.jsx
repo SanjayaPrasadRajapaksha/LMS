0712bd7b14cd0fa043b4917a02d0eda9f89d4757
Sanjaya Prasad <130231733+SanjayaPrasadRajapaksha@users.noreply.github.com>
2025-04-26 21:39:36 +0530

    Complete course list page implementation

--- client/src/components/student/Footer.jsx
+++ client/src/components/student/Footer.jsx
@@ -21,10 +21,10 @@ const Footer = () => {
         <div className='hidden md:flex flex-col items-start w-full'>
           <h2 className='font-semibold text-white mb-5 '>Subscribe to our newsletter</h2>
           <p className='text-sm text-white/80'>The latest news, articles, and resources, sent to your inbox weekly.</p>
-        <div className='flex items-center gap-2 pt-4'>
-          <input type="email" placeholder='Enter your email' className='boder border-gray-500/30 bg-gray-800 text-gray-500 placeholder-gray-500 outline-none w-64 h-9 rounded px-2 text-sm'/>
-          <button className='bg-blue-600 w-24 h-9 text-white rounded'>Subcribe</button>
-        </div>
+          <div className='flex items-center gap-2 pt-4'>
+            <input type="email" placeholder='Enter your email' className='boder border-gray-500/30 bg-gray-800 text-gray-500 placeholder-gray-500 outline-none w-64 h-9 rounded px-2 text-sm' />
+            <button className='bg-blue-600 w-24 h-9 text-white rounded'>Subcribe</button>
+          </div>
         </div>
       </div>
       <p className='py-4 text-center text-xs md:text-white/60'>Copyright 2024 © GreatStack. All Right Reserved.</p>
