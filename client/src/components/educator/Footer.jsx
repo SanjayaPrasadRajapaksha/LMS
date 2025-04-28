2d6322e1d47ba9f09cf2a21d8f48bd184b0eb84f
Sanjaya Prasad <130231733+SanjayaPrasadRajapaksha@users.noreply.github.com>
2025-04-28 09:08:27 +0530

    Create left section of educator footer

--- client/src/components/educator/Footer.jsx
+++ client/src/components/educator/Footer.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import { assets } from '../../assets/assets'
 
 const Footer = () => {
   return (
-    <div>
-      <h1>Edu Footer</h1>
-    </div>
+    <footer className='flex md:flex-row flex-col-reverse items-center justify-between
+    text-left w-full px-8 border-t'>
+      <div>
+        <img src={assets.logo} className='hidden md:block w-20' alt="logo" />
+      <div className='hidden md:block h-7 w-px bg-gray-500/60'></div>
+      <p className='py-4 text-center text-xs md:text-sm text-gray-500'>
+      Copyright 2024. All Right Reserved. 
+      </p>
+      </div>
+      
+    </footer>
   )
 }
 
